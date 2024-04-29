a21cf444e07ae1c8a9bb4ed3eb04d168d81482c7
Hanah Silva e Siqueira <160792664+hanahses@users.noreply.github.com>
2024-04-29 20:20:17 -0300

    Add files via upload

--- bro_code1.cpp
+++ bro_code1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+//using namespace std;
 
 //função main
 int main(){
@@ -20,17 +20,47 @@ int main(){
     // \n (quebra de linha)
     // std::endl; (quebra de linha com limpeza de buffer)
 
+    //INTEIROS
     int x; // valor inteiro x
     x = 5; //a variável x recebe o valor 5
     int y = 7; //a variável y recebe o valor 7 
     int soma = x + y;
     int sub = y - x;
-
-
+    
     std::cout << x << '\n';
     std::cout << y << '\n';
     std::cout << soma << '\n';
     std::cout << sub << '\n';
+    
+    
+    //DOUBLE - Valores decimais
+    double xd = 10.1; 
+    double yd = 5.2;
+    double multi = xd*yd; //multiplicação entre dois doubles
+    double div = xd/yd;
+
+    std::cout << multi << '\n';
+    std::cout << div << '\n';
+
+    //CHAR - Caracter
+    char a = 'A';
+
+    std::cout << a << '\n';
+
+    //BOOLEAN - Verdadeiro ou falso
+    bool luz = true;
+
+    std::cout << luz << '\n';
+
+    //STRING - Conjunto de caracteres
+
+    std::string nome = "Hanah"; //atribuindo a variavel nome(uma string) o "valor" Hanah
+    
+    std::cout << nome << '\n';
+
+    
+
+    
 
     return 0;
 }
